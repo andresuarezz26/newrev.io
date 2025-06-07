b06af9413b1512350326fc11e46818587e0cf56d
Gerardo Suárez <andresuarezz26@hotmail.com>
2025-06-07 10:41:33 -0500

    remove the use client ussage

--- client/src/components/ChatInterface.jsx
+++ client/src/components/ChatInterface.jsx
@@ -1,5 +1,3 @@
-"use client"
-
 import { useState, useEffect, useRef } from "react"
 import { Button, TextField, Paper, Typography, Box, Divider, CircularProgress, IconButton } from "@mui/material"
 import SendIcon from "@mui/icons-material/Send"
